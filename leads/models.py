69848f7831c8c4a1fd380843628f6451e202ea88
rampol62 <rampol62@hotmail.com>
2021-01-14 18:53:23 -0500

    hasta Models

--- leads/models.py
+++ leads/models.py
@@ -1,22 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    pass 
+
 
 class Lead(models.Model):
-
-    SOURCE_CHOICES = (
-        ('Youtube', 'YouTube'),
-        ('Google', 'Google'),
-        ('Newsletter', 'Newsletter'),
-    )
-
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
 
-    phoned = models.BooleanField(default=False)
-    source = models.CharField(choices=SOURCE_CHOICES, max_length=100,null=True)
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
-    profile_picture = models.ImageField(blank=True, null=True)
-    special_files = models.FileField(blank=True, null=True)
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.email
+
+
+
+
+    # first_name = models.CharField(max_length=20)
+    # last_name = models.CharField(max_length=20)
+
+
+
+        # SOURCE_CHOICES = (
+    #     ('Youtube', 'YouTube'),
+    #     ('Google', 'Google'),
+    #     ('Newsletter', 'Newsletter'),
+    # )
+
+
+    # phoned = models.BooleanField(default=False)
+    # source = models.CharField(choices=SOURCE_CHOICES, max_length=100,null=True)
+    # profile_picture = models.ImageField(blank=True, null=True)
+    # special_files = models.FileField(blank=True, null=True)
 
