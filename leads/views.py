f48b2c8597d0c39cdf3255a2b8b00ae39467cfce
rampol62 <rampol62@hotmail.com>
2021-01-21 18:51:18 -0500

    Antes de Extending Templates

--- leads/views.py
+++ leads/views.py
@@ -5,16 +5,17 @@ from .forms import LeadForm, LeadModelForm
 
 # Create your views here.
 
+
 def lead_list(request):
     lideres = Lead.objects.all()
     contexto01 = {
         "lideres": lideres
     }
-    context = {
-        "name":"joe",
-        "age":35
-    }
-    return render(request,"leads/lead_list.html",contexto01)
+    # context = {
+    #     "name": "joe",
+    #     "age": 35
+    # }
+    return render(request, "leads/lead_list.html", contexto01)
 
 
 def lead_detail(request, pk):
@@ -24,7 +25,7 @@ def lead_detail(request, pk):
     contexto = {
         "lider": lider
     }
-    return render(request,"leads/lead_detail.html",contexto)
+    return render(request, "leads/lead_detail.html", contexto)
 
 
 def lead_create(request):
@@ -33,12 +34,12 @@ def lead_create(request):
     if request.method == "POST":
         forma = LeadModelForm(request.POST)
         if forma.is_valid():
-           forma.save()
-           return redirect("/leads")
+            forma.save()
+            return redirect("/leads")
     contexto = {
-        "forma":forma
+        "forma": forma
     }
-    return render(request,"leads/lead_create.html",contexto)  
+    return render(request, "leads/lead_create.html", contexto)
 
 
 def lead_update(request, pk):
@@ -55,12 +56,11 @@ def lead_update(request, pk):
     }
     return render(request, "leads/lead_update.html", contexto)
 
+
 def lead_delete(request, pk):
-     lider = Lead.objects.get(id=pk)
-     lider.delete()
-     return redirect("/leads")
-
-
+    lider = Lead.objects.get(id=pk)
+    lider.delete()
+    return redirect("/leads")
 
 
 # def lead_create(request):
